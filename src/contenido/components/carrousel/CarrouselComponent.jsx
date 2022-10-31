bede3ec204e8f2e57ce57304943cff10b227ad0c
leonardo arena <leonardofarena@gmail.com>
2022-10-31 13:23:44 -0300

    nnn

--- src/contenido/components/carrousel/CarrouselComponent.jsx
+++ src/contenido/components/carrousel/CarrouselComponent.jsx
@@ -8,13 +8,13 @@ export const CarrouselComponent = () => {
     <Carousel.Item  >
     <img
         className="d-block w-100"
-        src="https://github.com/leofa741/contadoraturdera/blob/master/src/assets/images/bannercont2.webp?raw=true"
+        src="https://github.com/leofa741/contadoraturdera/blob/master/src/assets/images/contador-publico-calculo_9843-400x267.jpg?raw=true"
         alt="Second slide"
       />
 
       <Carousel.Caption>
-        <h3>First slide label</h3>
-        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        <h3>ASESORAMIENTO CONTABLE</h3>
+        <p>Estudio Contable e Impositivo MG & Asoc</p>
       </Carousel.Caption>
     </Carousel.Item>
     <Carousel.Item>
@@ -25,21 +25,21 @@ export const CarrouselComponent = () => {
       />
 
       <Carousel.Caption>
-        <h3>Second slide label</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <h3>ASESORAMIENTO CONTABLE</h3>
+        <p>Servicios Contables, Impositivos, Generales</p>
       </Carousel.Caption>
     </Carousel.Item>
     <Carousel.Item>
       <img
         className="d-block w-100"
-        src="https://github.com/leofa741/contadoraturdera/blob/master/src/assets/images/gmasoc.jpg?raw=true"
+        src="https://github.com/leofa741/contadoraturdera/blob/master/src/assets/images/Contador-público-como-revisor-fiscal.webp?raw=true"
         alt="Third slide"
       />
 
       <Carousel.Caption>
-        <h3>Third slide label</h3>
+        <h3>ASESORAMIENTO CONTABLE</h3>
         <p>
-          Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          Servicios Societarios, Laborales, Administrativos
         </p>
       </Carousel.Caption>
     </Carousel.Item>
