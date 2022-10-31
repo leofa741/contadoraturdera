142e8f6c2f6606f695a74a47fe9949e11e209c9a
leonardo arena <leonardofarena@gmail.com>
2022-10-31 10:54:16 -0300

    nn

--- src/contenido/components/carrousel/CarrouselComponent.jsx
+++ src/contenido/components/carrousel/CarrouselComponent.jsx
@@ -7,7 +7,7 @@ export const CarrouselComponent = () => {
     <Carousel.Item>
       <img
         className="d-block w-100"
-        src=".7contadoraturdera/src/assets/images/about-banner-img.jpg"
+        src="./contadoraturdera/src/assets/images/about-banner-img.jpg"
         alt="First slide"
       />
       <Carousel.Caption>
