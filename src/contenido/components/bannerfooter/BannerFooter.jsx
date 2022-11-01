3a83460690d98bbe0eed478449d0cdba10ff6ca1
leonardo arena <leonardofarena@gmail.com>
2022-11-01 16:59:50 -0300

    nnn

---
+++ src/contenido/components/bannerfooter/BannerFooter.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+export const BannerFooter = () => {
+  return (
+    <>
+
+<div className="our_partners_area bg_gradient_team">
+         <div className="book_now_aera ">
+            <div className="container">
+              <div className="row book_now">
+                  <div className="col-md-7 booking_text">
+                     <h4>
+                        Estudio Contable e Impositivo <br/>MG & Asoc
+                     </h4>
+                     <p>
+                     Nuestra misión es brindar un servicio integral de calidad, con un trato personalizado y cercano, para que nuestros clientes puedan concentrarse en su actividad principal.
+                     </p>
+                  </div>
+                  <div className="col-md-5 p0 book_bottun">
+                     <div className="col-md-5">
+                     </div>
+                     <div className="col-md-7">
+                        <div className="top-banner wow fadeInRight text-left" >
+                           <a id="#services" href="
+                           https://api.whatsapp.com/send?phone=-5491137883486&text=Hola%20MG%20Asoc%20me%20gustaria%20saber%20mas%20sobre%20sus%20servicios
+                         
+                           " className="btn btn-primary radius-50  wow fadeInUp  js-scroll-trigger" data-wow-delay="0.5s" >Free Consultation</a>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+    </>
+
+  )
+}
--- src/contenido/pages/Home.jsx
+++ src/contenido/pages/Home.jsx
@@ -1,7 +1,7 @@
 
 
 import React from 'react'
-import { CarrouselComponent } from '../components';
+import { BannerFooter, CarrouselComponent } from '../components';
 
 import { ConentLayout } from '../layout/ConentLayout';
 
@@ -13,23 +13,38 @@ export const Home = () => {
    <CarrouselComponent />
 
    <ConentLayout  
-    title="Home Page"
-    descrp_1="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl vel tincidunt lacinia, nisl nisl aliquam mauris, eget aliquam nisl nunc vel mauris. Sed euismod, nisl vel tincidunt lacinia, nisl nisl aliquam mauris, eget aliquam nisl nunc vel mauris."
-    descrip_2="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl vel tincidunt lacinia, nisl nisl aliquam mauris, eget aliquam nisl nunc vel mauris. Sed euismod, nisl vel tincidunt lacinia, nisl nisl aliquam mauris, eget aliquam nisl nunc vel mauris."
-    descrip_block="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl vel tincidunt lacinia, nisl nisl aliquam mauris, eget aliquam nisl nunc vel mauris. Sed euismod, nisl vel tincidunt lacinia, nisl nisl aliquam mauris, eget aliquam nisl nunc vel mauris."
+    title="MG & Asoc"
+    descrip_1="Nos dedicamos a la contabilidad y asesoramiento de empresas y particulares."
+    descrip_2="Estudio Contable e Impositivo MG & Asoc
+    Somos un estudio contable e impositivo con más de 20 años de experiencia en el mercado. Nuestro objetivo es brindar un servicio integral de calidad, con un trato personalizado y cercano, para que nuestros clientes puedan concentrarse en su actividad principal."
+    descrip_block="
+    Estudio Contable e Impositivo MG & Asoc  "
    >
 
-   
-<ul>
-                                    <li>Sed ut perspiciatis unde omnis</li>
-                                    <li>Lingues es membres del sam familie</li>
-                                    <li>Lorem ipsum dolor sit amet</li>
-                                    <li>This is Lorem ipsum dolor sit amet</li>
-                                    <li>consectetur adipisicing elit</li>
-                                </ul>
-   
+<div  id="about"  className="bg_gray py-70">
+         <div className="container">
+            <div className="row ">
+              
+               <div className="who_we_area title text-left col-md-6 col-sm-6 col-4pad wow fadeInUp">
+                  <h1 className="PT-90">  
+                      Quienes Somos
+                    </h1>
+                  <div className="hed_img"> 
+                     <img src="https://github.com/leofa741/contadoraturdera/blob/master/src/assets/images/headding-img.png?raw=true"/>
+                  </div>
+                  <p className="about-bottom-s  wow fadeInUp" >Somos un estudio joven, emprendedor, compuesto por profesionales con distintas especializaciones, que nace para acompañar a la pymes y pequeños negocios conocidos como “start ups” que no cuentan una estructura administrativa – contable o que están iniciando actividade</p>
+               
+               </div>
+               <div className="who_we_area col-md-6 col-sm-6 col-4pad wow fadeInUp">
+                  <img src="https://github.com/leofa741/contadoraturdera/blob/master/src/assets/images/s_b.png?raw=true"/>
+               </div>
+            </div>
+         </div>
+      </div>
+
 
    </ConentLayout>   
+   <BannerFooter  /> 
    </>
   )
 }
