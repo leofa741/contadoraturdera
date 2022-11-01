49b8ed97b996a77c43bfe49af29869d6abd259a2
leonardo arena <leonardofarena@gmail.com>
2022-11-01 10:38:29 -0300

    nnn

--- src/contenido/components/banner/BannerComponent.jsx
+++ src/contenido/components/banner/BannerComponent.jsx
@@ -8,10 +8,12 @@ import "./bannerstyless.css";
 export const BannerComponent = () => {
 
   return (
-    <div className="banner bannerAnimation">
+    <div className="banner ">
     <div className=" small-text ">MG & Asoc </div>
 
-    
+   <a > Nos dedicamos a la contabilidad y asesoramiento de empresas y particulares.  </a>
+
+  
    
    </div>
     
