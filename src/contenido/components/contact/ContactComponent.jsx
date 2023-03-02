7dffdf8776e3491004f879c66955bb3b18785e6f
leonardo arena <leonardofarena@gmail.com>
2023-03-02 10:15:31 -0300

    bbb

--- src/contenido/components/contact/ContactComponent.jsx
+++ src/contenido/components/contact/ContactComponent.jsx
@@ -54,7 +54,7 @@ export const ContactComponent = () => {
                                         <p><MailOutlineIcon
                                         style={{ fontSize: 20 }}
                                         color="primary"
-                                        /> <strong>Email:</strong> <a href='mailto:estudiomg_asociados@outlook.com' > "estudiomg_asociados@outlook.com"</a></p>
+                                        /> <strong>Email:</strong> <a href='mailto:estudiomg_asociados@outlook.com' > </a>"estudiomg_asociados@outlook.com"</p>
                                     </li>
                                 </ul>
                             </div>
